827ff95a30cbb0a6903d77e00489bf647c585609
Nischal Gautam <nischal1024@gmail.com>
2017-11-22 14:36:56 +1100

    changes

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import {RootApp} from './native-code/screen/';
+import {View, Text} from 'react-native';
 
-AppRegistry.registerComponent('nativeAndWeb', () => App);
+
+AppRegistry.registerComponent('nativeAndWeb', () => <View> <Text> Hello </Text></View>);
+
--- native-code/screen/RootApp.js
+++ native-code/screen/RootApp.js
@@ -1,10 +1,8 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
-// import {store} from '../../client/views/store';
-// import {App} from '../../client/views/app';
-
 import {store} from '../../client/views/store';
-import {App} from '../../client/views/app';
+import App from '../../App';
+
 class RootApp extends Component {
 constructor(props){
   super(props);
